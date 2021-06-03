558257c8bcac04e1f6deca16fb0ec688aadc569b
Lucas de Castro <lucas.rcastro1@gmail.com>
2021-06-03 13:00:15 -0300

    ajuste add class fade-in

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const sing_up_btn = document.querySelector('#sign-up-button');
 const container = document.querySelector('.container');
 const form = document.querySelector('.signin-signup')
 sing_up_btn.addEventListener('click', () => {
-    form.classList.remove('fade-in');
     container.classList.add('sign-up-mode');
     form.classList.add('fade-in');
+    setTimeout(() => {
+        form.classList.remove('fade-in');
+    }, 1500);
 });
 
 sing_in_btn.addEventListener('click', () => {
-    form.classList.remove('fade-in');
-    container.classList.remove('sign-up-mode');
     form.classList.add('fade-in');
+    container.classList.remove('sign-up-mode');
+    setTimeout(() => {
+        form.classList.remove('fade-in');
+    }, 1500);
 });
